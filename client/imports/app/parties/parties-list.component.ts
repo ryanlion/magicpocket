c002fa4c9089ac63f7c627ecfca69b76670d5b37
ryanlion <ryansc1983@gmail.com>
2016-12-21 23:41:41 +0800

    ....

--- client/imports/app/parties/parties-list.component.ts
+++ client/imports/app/parties/parties-list.component.ts
@@ -1,23 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
  
 import { Parties } from '../../../../both/collections/parties.collection';
 import { Party } from '../../../../both/models/party.model';
  
 import template from './parties-list.component.html';
+
+import { Subscription } from 'rxjs/Subscription';
+import { MeteorObservable } from 'meteor-rxjs';
  
 @Component({
   selector: 'parties-list',
   template
 })
-export class PartiesListComponent {
+export class PartiesListComponent implements OnInit, OnDestroy {
   parties: Observable<Party[]>;
+  partiesSub: Subscription;
  
   constructor() {
     this.parties = Parties.find({}).zone();
   }
- 
+
+  ngOnInit() {
+    this.parties = Parties.find({}).zone();
+    this.partiesSub = MeteorObservable.subscribe('parties').subscribe();
+  }
+
   removeParty(party: Party): void {
     Parties.remove(party._id);
+  }
+
+  ngOnDestroy() {
+    this.partiesSub.unsubscribe();
   }
 }
--- server/main.ts
+++ server/main.ts
@@ -1,4 +1,5 @@
 import { Main } from "./imports/server-main/main";
+import './imports/publications/parties';
 
 const mainInstance = new Main();
 mainInstance.start();
