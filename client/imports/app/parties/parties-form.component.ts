0ef12a7453a9886e833e4f19c3bccf01abd184e7
ryanlion <shi02022211@hotmail.com>
2016-12-22 16:43:38 +0800

    .....

--- client/imports/app/parties/parties-form.component.ts
+++ client/imports/app/parties/parties-form.component.ts
@@ -24,7 +24,8 @@ export class PartiesFormComponent implements OnInit {
     this.addForm = this.formBuilder.group({
       name: ['', Validators.required],
       description: [],
-      location: ['', Validators.required]
+      location: ['', Validators.required],
+      public: [false]
     });
   }
   addParty(): void {
